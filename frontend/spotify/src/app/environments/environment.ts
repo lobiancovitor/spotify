9bb439fa96432ca2f46646da8b6fa209cda40486
lobiancovitor <119963373+lobiancovitor@users.noreply.github.com>
2024-06-10 10:10:19 -0300

    Update environment.ts

--- frontend/spotify/src/app/environments/environment.ts
+++ frontend/spotify/src/app/environments/environment.ts
@@ -2,5 +2,5 @@ import { HttpHeaders } from "@angular/common/http";
 
 export const environment = {
     apiUrlBase: "https://spotify-cloud-api.azurewebsites.net/",
-    headers: new HttpHeaders({"ocp-apim-subscription-key":"dcae0f4c6d964dd88fb433a21f48ea24"})
+    headers: new HttpHeaders({"ocp-apim-subscription-key":"0eeca8c734a94b3d8ed377ad7a942881"})
 }
