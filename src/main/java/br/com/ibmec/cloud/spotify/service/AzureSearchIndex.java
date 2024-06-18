4b20c3329bd3cd2bee27b5b6c17b9ec4547d596b
lobiancovitor <119963373+lobiancovitor@users.noreply.github.com>
2024-06-18 09:11:52 -0300

    Update AzureSearchIndex.java

--- src/main/java/br/com/ibmec/cloud/spotify/service/AzureSearchIndex.java
+++ src/main/java/br/com/ibmec/cloud/spotify/service/AzureSearchIndex.java
@@ -8,6 +8,30 @@ public class AzureSearchIndex {
     private String id;
     private String banda;
 
-    @JsonProperty(value = "nome_musicas")
+    @JsonProperty(value = "nome_musica")
     private String nomeMusica;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBanda() {
+        return banda;
+    }
+
+    public void setBanda(String banda) {
+        this.banda = banda;
+    }
+
+    public String getNomeMusica() {
+        return nomeMusica;
+    }
+
+    public void setNomeMusica(String nomeMusica) {
+        this.nomeMusica = nomeMusica;
+    }
 }
