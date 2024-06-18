264ab85076301e11875e4de13518fb15e25637f2
lobiancovitor <119963373+lobiancovitor@users.noreply.github.com>
2024-06-18 10:42:41 -0300

    404

--- frontend/spotify/src/app/app.routes.ts
+++ frontend/spotify/src/app/app.routes.ts
@@ -1,8 +1,12 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login/login.component';
 import { HomeComponent } from './home/home.component';
 import { UserAutenticatedGuard } from './guard/user-autenticated.guard';
 import { FavoritoComponent } from './favorito/favorito.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     {
@@ -26,3 +30,13 @@ export const routes: Routes = [
 
     },
 ];
+
+@NgModule({
+    imports: [
+      BrowserModule,
+      FormsModule,
+      RouterModule.forRoot(routes, { useHash: true })
+    ],
+    bootstrap: [AppComponent]
+  })
+  export class AppModule { }
